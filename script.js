28e459629db3e8f12330062ae5bf2865b295a4bb
BurnyLlama <fabianseluska@gmail.com>
2019-09-15 14:10:01 +0200

    CSS and JS optimizations

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
   function showHome() {
-    document.getElementById('home').style.cssText = "border: .1rem solid #4b4bff;";
+    document.getElementById('home').style.cssText = "background: #4b4bff22;";
     document.getElementById('faq').style.cssText = "border: 0;";
     document.getElementById('contact').style.cssText = "border: 0;";
     document.getElementById('gp').style.cssText = "border: 0;";
@@ -10,7 +10,7 @@
   };
   function showFAQ() {
     document.getElementById('home').style.cssText = "border: 0;";
-    document.getElementById('faq').style.cssText = "border: .1rem solid #4b4bff;";
+    document.getElementById('faq').style.cssText = "background: #4b4bff22;";
     document.getElementById('contact').style.cssText = "border: 0;";
     document.getElementById('gp').style.cssText = "border: 0;";
     document.querySelector('.home').style.cssText = "display: none;";
@@ -21,7 +21,7 @@
   function showContact() {
     document.getElementById('home').style.cssText = "border: 0;";
     document.getElementById('faq').style.cssText = "border: 0;";
-    document.getElementById('contact').style.cssText = "border: .1rem solid #4b4bff;";
+    document.getElementById('contact').style.cssText = "background: #4b4bff22;";
     document.getElementById('gp').style.cssText = "border: 0;";
     document.querySelector('.home').style.cssText = "display: none;";
     document.querySelector('.faq').style.cssText = "display: none;";
@@ -32,9 +32,31 @@
     document.getElementById('home').style.cssText = "border: 0;";
     document.getElementById('faq').style.cssText = "border: 0;";
     document.getElementById('contact').style.cssText = "border: 0;";
-    document.getElementById('gp').style.cssText = "border: .1rem solid #4b4bff;";
+    document.getElementById('gp').style.cssText = "background: #4b4bff22;";
     document.querySelector('.home').style.cssText = "display: none;";
     document.querySelector('.faq').style.cssText = "display: none;";
     document.querySelector('.contact').style.cssText = "display: none;";
     document.querySelector('.gp').style.cssText = "display: ;";
+  };
+
+  function darkMode() {
+    if (document.getElementById('darkmode').checked == true) {
+      document.body.style.cssText += "background: black; color: white; transition: .3s;";
+    } else {
+      document.body.style.cssText += "background: white; color: black; transition: .3s;";
+    };
+  };
+  function serifs() {
+    if (document.getElementById('serifs').checked == true) {
+      document.body.style.cssText += "font-family: 'NotoSerif'; transition: .3s;";
+    } else {
+      document.body.style.cssText += "font-family: 'NotoSans'; transition: .3s;";
+    };
+  };
+  function largeText() {
+    if (document.getElementById('largetext').checked == true) {
+      document.body.style.cssText += "font-size: 16pt; transition: .3s;";
+    } else {
+      document.body.style.cssText += "font-size: 12pt; transition: .3s;";
+    };
   };
